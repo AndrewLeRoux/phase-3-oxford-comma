04ea9b7d7549b6e93db77e1cfc3cae4880044222
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-04-12 11:03:37 -0700

    commit tree 90108b43ae70dc82bd191501d15761c8d0bd6bbe

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    str = ""
+    if array.length < 3
+        str = array.join(" and ")
+    else  
+        array.each.with_index do |el, index|
+            if index < array.length - 1
+                str << el + ", "
+            else
+                str << "and " + el
+            end
+        end
+    end
+    str
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
